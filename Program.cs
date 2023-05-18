09d8b745b1392f14119ce8e07440f9cf0a969533
BaLTaZaR <gricaev_sergey@mail.ru>
2023-05-18 15:03:43 +0600

    создал новый массив с необходимым количеством ячеек

--- Program.cs
+++ Program.cs
@@ -2,19 +2,23 @@
 
 Console.WriteLine("Задайте количество элементов массива: ");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
-string[] array = new string[sizeArray];
+string[] array = new string[sizeArray]; // создаём массив
 
-for (int i = 0; i < sizeArray; i++)
+for (int i = 0; i < sizeArray; i++) // помещаем в массив элементы, введённые пользователем
 {
 	Console.Write($"Введите {i + 1}-й элемент массива: ");
 	string arrElem = Convert.ToString(Console.ReadLine()!);
 	array[i] = arrElem;
 }
 int count = 0;
-foreach (string arrElem in array)
+foreach (string arrElem in array) // проверяем на ограничение по символам каждого элемента
 {
 	if (array.Length <= 3)
 	{
 		count++;
 	}
 }
+string[] newArray = new string[count]; //создаём новый массив с необходимым количеством ячеек
+
+
+
