ee7de6a3629eb9c274d346b80461161cf10edcc7
BaLTaZaR <gricaev_sergey@mail.ru>
2023-05-18 14:58:02 +0600

    создал массив и проверил на ограничение символов, согласно условию задачи

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Задайте количество элементов массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[sizeArray];
+
+for (int i = 0; i < sizeArray; i++)
+{
+	Console.Write($"Введите {i + 1}-й элемент массива: ");
+	string arrElem = Convert.ToString(Console.ReadLine()!);
+	array[i] = arrElem;
+}
+int count = 0;
+foreach (string arrElem in array)
+{
+	if (array.Length <= 3)
+	{
+		count++;
+	}
+}
